fee2101a353394930a12260132ab5890d8c367ba
nGray22 <noah.gray@stu.daviess.kyschools.us>
2025-02-11 15:58:17 +0000

    everything looks good but i cant seem to fing out where i went wrong

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,21 +9,59 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
-    
+    for(int row=0; row<ray.length; row++)
+    {
+      int count = 0;
+     for(int col = 0;col<ray[row].length; col++)
+     {
+      count += ray[row][col]; 
+
+     }
+     count= count/ray.length;
+    }  
   }
 
   public void columnAverages(){
-
+    //column major order, take each column 
+    //and go through all the rows top down 
+    //and left to right
+    //2 changes, column on outside row on inside
+    //row needs to change to 0
+    for (int i=0; i <= ray.length; i++)
+    for(int col=0; col<ray[i].length; col++)
+    {
+      int count = 0;
+     for(int row = 0;col<ray.length; row++)
+     {
+    
+      count += ray[row][col]; 
+     
+     }
+     count=count/ray[0].length;
+    }
   }
 
   public void arrayAverage(){
-
+    int count = 0;
+    int tot = ray.length + ray[0].length;
+    for(int[] row: ray)
+    { 
+      for(int col: row)
+      {
+      count += col;
+      }
+    }
+    count = count/tot;
+    
   }
+
 
   public String toString(){
     for(int row=0; row<ray.length; row++)
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+    return ""; 
+  
   }
 }
--- Gee17.java
+++ Gee17.java
@@ -1,7 +1,27 @@
 public class Gee17{
   public static void main(String[] args){
-    TwoDee twoDee = new TwoDee();
+    
+    int[][] ray = new int[7][7];
 
+    //populate the array with sequential numbers
+    //1 2 3 4 5 6 7
+    //8 9 10 11 12 13 14 
+    //15 16 17 18 19 20 21...
+    //for(int i =0; i<= ray.length; i++)
+    for (int row=0; row<ray.length; row++)
+    {   
+      for (int column=0; column<ray[row].length; column++)
+      { 
+        ray[row][column]=column+row*ray.length+1;
+        
+      }
+    }
+
+
+
+    ArrayAverages twoDee = new ArrayAverages(ray);
+
+    System.out.println(twoDee);
     twoDee.rowAverages();
     twoDee.columnAverages();
     twoDee.arrayAverage();
